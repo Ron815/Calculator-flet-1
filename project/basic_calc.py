4751c0484d1fe48ab44f767e3730ba5cc74364ab
Ron Shiue <219948048+Ron815@users.noreply.github.com>
2026-07-28 21:43:06 -0700

    rewrite the structure

--- project/basic_calc.py
+++ project/basic_calc.py
@@ -2,146 +2,119 @@ import flet as ft
 from decimal import Decimal
 
 def main(page: ft.Page):
-    number_type=["0","1","2","3","4","5","6","7","8","9","."]
-    symbol_type=["+", "-", "*", "/", "^"]
-    number1=[]  #list
-    n=[]  #list
-    s=[]
-    symbol=""
-    calculation=ft.Text(value="", size=15)  
+    number_type=["0","1","2","3","4","5","6","7","8","9"]
+    symbol_type=["+", "-", "*", "/", "^","."]
+    current_number=[]  #list
+    numbers=[]  #list
+
+    symbols=[]
+    calculation_back=[]
+    calculation_show=ft.Text(value="", size=15)  
     result=ft.Text(value="", size=20)
+
+    calc = {
+        "+": lambda a,b : a+b,
+        "-": lambda a,b : a-b,
+        "*": lambda a,b : a*b,
+        "^": lambda a,b : a**b
+        }
+
+    def clicked_show(e):
+        enter=e.control.content
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        if enter in number_type:
+            current_number.append(enter)
+            calculation_back.append(enter)
+
+        elif enter in symbol_type and current_number != [] :
+
+            if calculation_back[-1] not in symbol_type:
+
+                if enter != ".":
+                    numbers.append("".join(current_number))
+                    current_number.clear()
+                    calculation_back.append(enter)
+
+                elif enter =="." and "." not in current_number :
+                    symbols.append(enter)
+                    current_number.append(enter)
+                    numbers.append(str(Decimal("".join(current_number))))
+                    calculation_back.append(enter)
+
+        calculation_show.value = "".join(calculation_back)
+        page.update()
+        
+    def clicked_calc():
+        result.value = eval("".join(calculation_back))
+        
 
     def convert(a):
         if a=="":
              return "0"
         return Decimal("".join(a))
 
-    def clicked(e):
-        enter=e.control.content
-        nonlocal symbol, number1, n, calculation, result, s
-        if enter in symbol_type and len(s)== len(number1) -1:
-                s.append(enter) 
-        
-        elif enter in number_type:
-            if symbol=="":
-                if enter==".":
-                    if number1 !=[] and number1.count(".")==0:
-                        number1.append(enter)
-                else:
-                    number1.append(enter)
 
-
-            else:
-                if enter==".":
-                    if n !=[] and n.count(".")==0:
-                        n.append(enter)
-                else:
-                    n.append(enter)
-               
-        a="".join(number1)
-        b="".join(n)
-        calculation.value = a + symbol + b
-        page.update()
-
-    def calc(): 
-        nonlocal symbol, number1, n, calculation, result
-        if number1!=[] and n !=[] and symbol!="":
-            if symbol == "+":
-                result.value = str(f"={convert(number1) + convert(n)}")
-            elif symbol == "-":
-                result.value = str(f"={convert(number1) - convert(n)}")
-            elif symbol == "*":
-                result.value = str(f"={convert(number1) * convert(n)}")
-            elif symbol == "/":
-                if str(convert(n))=="0":
-                    result.value="Error"
-                else:
-                    result.value = str(f"={convert(number1) / convert(n)}")
-            elif symbol == "^":
-                result.value = str(f"={convert(number1) ** convert(n)}")
-
-        elif number1!=[] and symbol=="":
-            result.value=str(convert(number1))
-
-        else:
-            result.value="Error"
-
-    def clear():
-        nonlocal symbol, number1, n, calculation, result 
-        number1.clear()
-        n.clear()
-        symbol=""
-        calculation.value=""
+    def clear(): #test: OK
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        current_number.clear()
+        numbers.clear()
+        symbols=[]
+        calculation_back=[]
+        calculation_show.value=""
         result.value=""
     
-    def back():
-        nonlocal symbol, number1, n, calculation, result
-        if n!=[]:
-            n.pop()
-            a="".join(number1)
-            b="".join(n)
-            result.value=""
-            calculation.value = a + symbol + b
+    def back(): #test: OK
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        if calculation_back!=[]:
+            calculation_back.pop()
+            calculation_show.value = "".join(calculation_back)
             page.update()
-            
-        elif symbol != "":
-            symbol=""
-            a="".join(number1)
-            b="".join(n)
-            calculation.value = a + symbol + b
-            page.update()
-            
-        elif number1 !=[]:
-            number1.pop()
-            a="".join(number1)
-            b="".join(n)
-            calculation.value = a + symbol + b
-            page.update()
-    
+                
 
     page.add(
-        calculation,
+        calculation_show,
         result,
         ft.Row(
             controls=[
-            ft.Button(content="1", on_click=clicked),
-            ft.Button(content="2", on_click=clicked),
-            ft.Button(content="3", on_click=clicked),
-            ft.Button(content="4", on_click=clicked),
+            ft.Button(content="1", on_click=clicked_show),
+            ft.Button(content="2", on_click=clicked_show),
+            ft.Button(content="3", on_click=clicked_show),
+            ft.Button(content="4", on_click=clicked_show),
         ]
             ),
             
     ft.Row(
             controls=[            
-            ft.Button(content="5", on_click=clicked),
-            ft.Button(content="6", on_click=clicked),
-            ft.Button(content="7", on_click=clicked),
-            ft.Button(content="8", on_click=clicked),
+            ft.Button(content="5", on_click=clicked_show),
+            ft.Button(content="6", on_click=clicked_show),
+            ft.Button(content="7", on_click=clicked_show),
+            ft.Button(content="8", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[            
-            ft.Button(content="9", on_click=clicked),
-            ft.Button(content="0", on_click=clicked),
-            ft.Button(content=".", on_click=clicked),
-            ft.Button(content="+", on_click=clicked),
+            ft.Button(content="9", on_click=clicked_show),
+            ft.Button(content="0", on_click=clicked_show),
+            ft.Button(content=".", on_click=clicked_show),
+            ft.Button(content="+", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
-            ft.Button(content="-", on_click=clicked),
-            ft.Button(content="*", on_click=clicked),
-            ft.Button(content="/", on_click=clicked),
-            ft.Button(content="^", on_click=clicked),
+            ft.Button(content="-", on_click=clicked_show),
+            ft.Button(content="*", on_click=clicked_show),
+            ft.Button(content="/", on_click=clicked_show),
+            ft.Button(content="^", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
-            ft.Button(content="=", on_click=calc),
+            ft.Button(content="=", on_click=clicked_calc),          
             ft.Button(content="AC", on_click=clear),
-            ft.Button(content="←", on_click= back)
-        ]
-            ))
+            ft.Button(content="←", on_click=back),     
+            ]
+                )
+                    )
     
 
 if __name__ == "__main__" :
--- project/test4.py
+++ project/test4.py
@@ -2,107 +2,112 @@ import flet as ft
 from decimal import Decimal
 
 def main(page: ft.Page):
-    number_type=["0","1","2","3","4","5","6","7","8","9","."]
-    symbol_type=["+", "-", "*", "/", "^"]
-    number1=[]
-    number2=[]
-    symbol=""
-    result=ft.Text(value="0")
+    number_type=["0","1","2","3","4","5","6","7","8","9"]
+    symbol_type=["+", "-", "*", "/", "^","."]
+    current_number=[]  #list
+    numbers=[]  #list
+
+    symbols=[]
+    calculation_back=[]
+    calculation_show=ft.Text(value="", size=15)  
+    result=ft.Text(value="", size=20)
+
+    def clicked_show(e):
+        enter=e.control.content
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        if enter in number_type:
+            current_number.append(enter)
+            calculation_back.append(enter)
+
+        elif enter in symbol_type and current_number != [] :
+
+            if calculation_back[-1] not in symbol_type:
+
+                if enter != ".":
+                    numbers.append("".join(current_number))
+                    current_number.clear()
+                    calculation_back.append(enter)
+
+                elif enter =="." and "." not in current_number :
+                    symbols.append(enter)
+                    current_number.append(enter)
+                    numbers.append(str(Decimal("".join(current_number))))
+                    calculation_back.append(enter)
+
+        calculation_show.value = "".join(calculation_back)
+        page.update()
+        
+    def clicked_calc():
+        result.value = eval("".join(calculation_back))
+        
 
     def convert(a):
         if a=="":
              return "0"
         return Decimal("".join(a))
 
-    def clicked(e):
-        enter=e.control.content
-        nonlocal symbol, number1, number2
-        if enter in symbol_type:
-                symbol=enter 
-        
-        elif enter in number_type:
-            if symbol=="":
-                if enter==".":
-                    if number1 !=[] and number1.count(".")==0:
-                        number1.append(enter)
-                else:
-                    number1.append(enter)
 
-
-            else:
-                if enter==".":
-                    if number2 !=[] and number2.count(".")==0:
-                        number2.append(enter)
-                else:
-                    number2.append(enter)
-               
-        
-        result.value = "".join(number1) + symbol + "".join(number2)
-        page.update()
-
-    def calc(): 
-        nonlocal symbol, number1, number2
-        if symbol == "+":
-            result.value = str(convert(number1) + convert(number2))
-        elif symbol == "-":
-            result.value = str(convert(number1) - convert(number2))
-        elif symbol == "*":
-           result.value = str(convert(number1) * convert(number2))
-        elif symbol == "/":
-            result.value = str(convert(number1) / convert(number2))
-        elif symbol == "^":
-            result.value = str(convert(number1) ** convert(number2))
-
-    def clear():
-        nonlocal symbol, number1, number2
-        number1.clear()
-        number2.clear()
-        symbol=""
-        result.value="0"
+    def clear(): #test: OK
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        current_number.clear()
+        numbers.clear()
+        symbols=[]
+        calculation_back=[]
+        calculation_show.value=""
+        result.value=""
+    
+    def back(): #test: OK
+        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+        if calculation_back!=[]:
+            calculation_back.pop()
+            calculation_show.value = "".join(calculation_back)
+            page.update()
+                
 
     page.add(
+        calculation_show,
         result,
         ft.Row(
             controls=[
-            ft.Button(content="1", on_click=clicked),
-            ft.Button(content="2", on_click=clicked),
-            ft.Button(content="3", on_click=clicked),
+            ft.Button(content="1", on_click=clicked_show),
+            ft.Button(content="2", on_click=clicked_show),
+            ft.Button(content="3", on_click=clicked_show),
+            ft.Button(content="4", on_click=clicked_show),
         ]
             ),
             
     ft.Row(
-            controls=[
-            ft.Button(content="4", on_click=clicked),
-            ft.Button(content="5", on_click=clicked),
-            ft.Button(content="6", on_click=clicked),
+            controls=[            
+            ft.Button(content="5", on_click=clicked_show),
+            ft.Button(content="6", on_click=clicked_show),
+            ft.Button(content="7", on_click=clicked_show),
+            ft.Button(content="8", on_click=clicked_show),
+        ]
+            ),
+    ft.Row(
+            controls=[            
+            ft.Button(content="9", on_click=clicked_show),
+            ft.Button(content="0", on_click=clicked_show),
+            ft.Button(content=".", on_click=clicked_show),
+            ft.Button(content="+", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
-            ft.Button(content="7", on_click=clicked),
-            ft.Button(content="8", on_click=clicked),
-            ft.Button(content="9", on_click=clicked),
+            ft.Button(content="-", on_click=clicked_show),
+            ft.Button(content="*", on_click=clicked_show),
+            ft.Button(content="/", on_click=clicked_show),
+            ft.Button(content="^", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
-            ft.Button(content="0", on_click=clicked),
-            ft.Button(content=".", on_click=clicked),
-            ft.Button(content="+", on_click=clicked),
-        ]
-            ),
-    ft.Row(
-            controls=[
-            ft.Button(content="-", on_click=clicked),
-            ft.Button(content="*", on_click=clicked),
-            ft.Button(content="/", on_click=clicked),
-        ]
-            ),
-    ft.Row(
-            controls=[
-            ft.Button(content="=", on_click=calc),
+            ft.Button(content="=", on_click=clicked_calc),          
             ft.Button(content="AC", on_click=clear),
-               ]))
+            ft.Button(content="←", on_click=back),     
+            ]
+                )
+                    )
     
 
 if __name__ == "__main__" :
