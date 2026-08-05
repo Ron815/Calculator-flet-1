8c8e39f8ce2430a557e79dca33aafbbd76ba3c16
Ron Shiue <219948048+Ron815@users.noreply.github.com>
2026-08-05 14:04:06 -0700

    fix problems on brackets and back button

--- project/basic_calc.py
+++ project/basic_calc.py
@@ -1,75 +1,169 @@
 import flet as ft
 from decimal import Decimal
 
+
 def main(page: ft.Page):
+    
     number_type=["0","1","2","3","4","5","6","7","8","9"]
     symbol_type=["+", "-", "*", "/", "^","."]
+    brackets_type=["(", ")"]
     current_number=[]  #list
-    numbers=[]  #list
-
-    symbols=[]
+    current_calculation=[]  #list
     calculation_back=[]
     calculation_show=ft.Text(value="", size=15)  
     result=ft.Text(value="", size=20)
+    calc_state=True
 
-    calc = {
-        "+": lambda a,b : a+b,
-        "-": lambda a,b : a-b,
-        "*": lambda a,b : a*b,
-        "^": lambda a,b : a**b
-        }
 
     def clicked_show(e):
         enter=e.control.content
-        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
-        if enter in number_type:
+        nonlocal current_number, current_calculation, calculation_back, calculation_show
+
+        if (enter in number_type and current_calculation==[]) or (enter in number_type and  current_calculation[-1]!=")"):
             current_number.append(enter)
-            calculation_back.append(enter)
+            current_calculation.append(enter)
 
-        elif enter in symbol_type and current_number != [] :
+        elif enter == "(" :
+            if current_calculation==[] or current_calculation[-1]=="(" or current_calculation[-1] in symbol_type:
+                calculation_back.append(enter)
+                current_calculation.append(enter)
 
-            if calculation_back[-1] not in symbol_type:
+        elif enter in symbol_type :
+            if current_calculation != [] :
 
-                if enter != ".":
-                    numbers.append("".join(current_number))
-                    current_number.clear()
+                if current_calculation[-1] in number_type:
+
+                    if enter != ".":
+                        calculation_back.append("".join(current_number))
+                        calculation_back.append(enter)
+                        current_number=[]
+                        current_calculation.append(enter)
+
+                    elif enter =="." and "." not in current_number :
+                        current_number.append(enter)
+                        current_calculation.append(enter)
+
+                if current_calculation[-1]==")":
+                    current_calculation.append(enter)
                     calculation_back.append(enter)
 
-                elif enter =="." and "." not in current_number :
-                    symbols.append(enter)
-                    current_number.append(enter)
-                    numbers.append(str(Decimal("".join(current_number))))
+        elif  (enter == ")") and (current_calculation!=[]) :
+            if  (current_calculation[-1] != "(") and (current_calculation[-1] not in symbol_type):
+                if current_calculation[-1] in number_type:
+                    calculation_back.append("".join(current_number))
                     calculation_back.append(enter)
+                    current_number=[]
+                    current_calculation.append(enter)        
 
-        calculation_show.value = "".join(calculation_back)
+                elif current_calculation [-1] == ")":        
+                    calculation_back.append(enter)
+                    current_calculation.append(enter)
+
+
+        calculation_show.value = "".join(current_calculation)
         page.update()
-        
+
     def clicked_calc():
-        result.value = eval("".join(calculation_back))
-        
 
-    def convert(a):
-        if a=="":
-             return "0"
-        return Decimal("".join(a))
+        if current_number!=[]:
+            calculation_back.append("".join(current_number))
+
+        def sort(index,ans,calculation):
+            calculation.pop(index)
+            calculation.pop(index)
+            calculation[index-1]=str(ans)
+
+        def calc_main(calculation, start_index="", end_index=""):
+            nonlocal calculation_back, calc_state
+            if calc_state==True:
+                while "^" in calculation:
+                    for index, value in enumerate(calculation):
+                        if value == "^":
+                            ans = Decimal(calculation[index-1]) ** Decimal(calculation[index+1])
+                            sort(index,ans, calculation)
+
+                while "*" in calculation or "/" in calculation : 
+                    for index,value in enumerate(calculation):
+                            if value == "*":
+                                ans = Decimal(calculation[index-1]) * Decimal(calculation[index+1])
+                                sort(index,ans,calculation)
+                            elif value == "/":
+                                try:
+                                    ans = Decimal(calculation[index-1]) / Decimal(calculation[index+1])
+                                    sort(index,ans,calculation)
+                                except :
+                                    result.value="Error"
+                                    calc_state=False
+                                    return "Error"
+
+                while "+" in calculation or "-" in calculation : 
+                    for index,value in enumerate(calculation):             
+                        if value == "+":
+                            ans = Decimal(calculation[index-1]) + Decimal(calculation[index+1])
+                            sort(index,ans,calculation)
+                        elif value == "-":
+                            ans = Decimal(calculation[index-1]) - Decimal(calculation[index+1])
+                            sort(index,ans,calculation)
+
+                if start_index != "" :
+                    del calculation_back[start_index+1 : end_index+1]
+                    calculation_back[start_index] = "".join(calculation)
+
+                result.value = "".join(calculation_back)
+
+        while calculation_back.count("(") > calculation_back.count(")"):
+            calculation_back.append(")")
+        while calculation_back.count("(") < calculation_back.count(")"):
+            calculation_back.insert(0,"(")
+
+        while "(" in calculation_back and calc_state==True :
+            for index, value in enumerate(calculation_back):
+                if value == "(":
+                    start_index=index
+                elif value == ")":
+                    end_index=index
+                    break
+            in_bracket = calculation_back[start_index+1 : end_index]
+            calc_main(in_bracket, start_index, end_index)
+
+        calc_main(calculation_back)
 
 
-    def clear(): #test: OK
-        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
+    def clear(): 
+        nonlocal current_number, current_calculation, calculation_back, result, calculation_show
         current_number.clear()
-        numbers.clear()
-        symbols=[]
-        calculation_back=[]
-        calculation_show.value=""
+        current_calculation.clear()
+        calculation_back.clear()
         result.value=""
+
+        calculation_show.value = "".join(current_calculation)
+        page.update()
     
-    def back(): #test: OK
-        nonlocal symbols, current_number, numbers, calculation_back, result, calculation_show
-        if calculation_back!=[]:
-            calculation_back.pop()
-            calculation_show.value = "".join(calculation_back)
+    def back(): 
+        nonlocal current_number, current_calculation, calculation_back, result, calculation_show, calc_state, brackets_type
+        result.value=""
+        calc_state=True
+        if current_calculation != []:
+            if current_number != []:
+                current_number.pop()
+
+            elif calculation_back != []:
+                if list(calculation_back[-1])[-1] in number_type or list(calculation_back[-1])[-1] ==".":
+                    current_number=list(calculation_back[-1])
+                    current_number.pop()
+                    calculation_back.pop()
+
+                elif (calculation_back[-1] in symbol_type) or (calculation_back[-1] in brackets_type):
+                    calculation_back.pop()
+                    if len(calculation_back) > 0 :
+                        if list(calculation_back[-1])[-1] in number_type or list(calculation_back[-1])[-1] =="." :
+                            current_number=list(calculation_back[-1])
+                            calculation_back.pop()
+
+            current_calculation.pop()
+            calculation_show.value = "".join(current_calculation)
             page.update()
-                
+
 
     page.add(
         calculation_show,
@@ -96,25 +190,32 @@ def main(page: ft.Page):
             ft.Button(content="9", on_click=clicked_show),
             ft.Button(content="0", on_click=clicked_show),
             ft.Button(content=".", on_click=clicked_show),
-            ft.Button(content="+", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
+            ft.Button(content="+", on_click=clicked_show),
             ft.Button(content="-", on_click=clicked_show),
             ft.Button(content="*", on_click=clicked_show),
             ft.Button(content="/", on_click=clicked_show),
-            ft.Button(content="^", on_click=clicked_show),
         ]
             ),
     ft.Row(
             controls=[
-            ft.Button(content="=", on_click=clicked_calc),          
-            ft.Button(content="AC", on_click=clear),
+            ft.Button(content="^", on_click=clicked_show),
+            ft.Button(content="(", on_click=clicked_show),
+            ft.Button(content=")", on_click=clicked_show),     
+            ft.Button(content="=", on_click=clicked_calc), 
+        ]
+            ),
+    ft.Row(
+        controls=[
+                     
             ft.Button(content="←", on_click=back),     
-            ]
+            ft.Button(content="AC", on_click=clear),
+        ]
+            )
                 )
-                    )
     
 
 if __name__ == "__main__" :
